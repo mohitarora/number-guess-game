9827d3c25a54f6dadc75443726af93a237c8ec9e
marora <mailtomohitarora@gmail.com>
2014-09-06 07:55:29 -0400

    Added spock test cases

--- src/test/groovy/com/sample/game/resources/ResultResourceTest.groovy
+++ src/test/groovy/com/sample/game/resources/ResultResourceTest.groovy
@@ -1,6 +1,9 @@
 package com.sample.game.resources
 
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.ws.rs.WebApplicationException
 
 /**
  * Unit test cases for Result Resource
@@ -8,14 +11,23 @@ import spock.lang.Specification
 
 class ResultResourceTest extends Specification {
 
-    def "10 should give back the valid result"() {
-        given:
-        def resource = new ResultResource(10)
-        when:
-        def result = resource.getResult(10)
-        then:
-        result
+    def resource = new ResultResource(10)
 
+    def "10 should give back the valid result"() {
+        expect:
+            resource.getResult(number)
+        where:
+            number << 10
+    }
+
+    @Unroll
+    def "#number should give back the invalid result"() {
+        when:
+            resource.getResult(number)
+        then:
+            thrown(WebApplicationException)
+        where:
+            number << [1,2,3,null,9,11,13]
     }
 
 }
